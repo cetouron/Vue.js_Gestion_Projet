dda094301e23475b50479ea9b8a068c2e8121a78
cetouron <cedric.touron@univ-lyon2.fr>
2021-01-05 16:33:32 +0100

    Your commit message

---
+++ src/views/Connexion.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row>
+ 
+        <Formulaire />
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Formulaire from '@/components/connexion/Formulaire.vue'
+
+export default {
+  components: {
+    Formulaire
+  }
+}
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row>
+     
+        <Register />
+ 
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Register from '@/components/register/Register.vue'
+
+export default {
+  components: {
+    Register
+  }
+}
+</script>
